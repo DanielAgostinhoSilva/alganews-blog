409a213851e7841f2be44ec669a2d992b68cc2d0
Daniel <daniel.agostinho@hotmail.com>
2022-10-27 10:04:57 -0300

    feat: 11.10 Criando o tema

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ThemeProvider} from "styled-components";
+import {light} from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={light}>
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
